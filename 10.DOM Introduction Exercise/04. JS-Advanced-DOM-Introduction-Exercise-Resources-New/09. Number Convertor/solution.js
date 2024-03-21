d4ece107050af05cc5d879d41f50f8b4d78a5371
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-03-21 14:08:46 +0100

    done with homework

--- 10.DOM Introduction Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ 10.DOM Introduction Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,38 @@
 function solve() {
+    const inputNumberElement = document.getElementById('input');
+    const resultElement = document.getElementById('result');
+    // const selectMenuFromElement = document.getElementById('selectMenuFrom');
+    const selectMenuToElement = document.getElementById('selectMenuTo');
+    const convertButtonElement = document.querySelector('button');
 
-    //TODO...
+    // Populate select menu to with options
+    const binaryOptionElement = selectMenuToElement.querySelector('option');
+    binaryOptionElement.value = 'binary';
+    binaryOptionElement.textContent = 'Binary';
+
+    const hexadecimlOptionElement = document.createElement('option');
+    hexadecimlOptionElement.value = 'hexadecimal';
+    hexadecimlOptionElement.textContent = 'Hexadecimal';
+    selectMenuToElement.appendChild(hexadecimlOptionElement);
+
+    const convertors = {
+        binary: convertDecimalToBinary,
+        hexadecimal: convertDecimalToHex,
+    };
+
+    convertButtonElement.addEventListener('click', () => {
+        const numberValue = Number(inputNumberElement.value);
+
+        resultElement.value = convertors[selectMenuToElement.value](numberValue)
+    });
+
+    // Just use toString(2)
+    function convertDecimalToBinary(number) {
+        return number.toString(2)
+    }
+    
+    // Just use toString(16)
+    function convertDecimalToHex(number = 10) {
+        return number.toString(16).toLocaleUpperCase();
+    }
 }
