da37ed323315d5f263d8c8e468692b5b5ae87fcf
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-04-05 22:15:22 +0200

    Finaly finished with the old homework!

--- 14.Asynchronous Programing/02.Bus-Schedule/app.js
+++ 14.Asynchronous Programing/02.Bus-Schedule/app.js
@@ -1,10 +1,37 @@
 function solve() {
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+
+    const infoSpanElement = document.querySelector('#info span.info');
+    const departButtonElement = document.getElementById('depart');
+    const arriveButtonElement = document.getElementById('arrive');
+    let current = 'depot';
+    let next = '';
+
     function depart() {
-        // TODO: 
+        fetch(`${baseUrl}/${current}`)
+            .then(res => res.json())
+            .then(data => {
+                infoSpanElement.textContent = `Next stop ${data.name}`
+                next = data.next;
+                departButtonElement.setAttribute('disabled', 'disabled')
+                arriveButtonElement.removeAttribute('disabled');
+            })
+            .catch(err => {
+                infoSpanElement.textContent = 'Error'
+            })
+
+            return fetch(`${baseUrl}/${current}`);
     }
 
     async function arrive() {
-        // TODO:
+      const result = await depart();
+      const stop = await result.json();
+
+      infoSpanElement.textContent = `Arriving at ${stop.name}`
+      current = next;
+      arriveButtonElement.setAttribute('disabled', 'disabled');
+      departButtonElement.removeAttribute('disabled');
+    
     }
 
     return {
--- 14.Asynchronous Programing/03.Forecaster/app.js
+++ 14.Asynchronous Programing/03.Forecaster/app.js
@@ -4,6 +4,7 @@ function attachEvents() {
     const submitButton = document.getElementById('submit');
     const forecastElement = document.getElementById('forecast');
     const currentElement = document.getElementById('current')
+    const upcomingElement = document.getElementById('upcoming');
 
     const wheaterSymbol = {
         'Sunny': '☀',
@@ -12,6 +13,65 @@ function attachEvents() {
         'Rain': '☂',
         'Degrees': '°',
     }
+
+    submitButton.addEventListener('click', async () => {
+        const locationResponse = await fetch(`${baseUrl}/locations`);
+        const locationData = await locationResponse.json();
+
+
+        const { code } = locationData.find(location => location.name === locationElement.value);
+
+        const todayResponse = await fetch(`${baseUrl}/today/${code}`);
+        const today = await todayResponse.json()
+        const upcomingResponse = await fetch(`${baseUrl}/upcoming/${code}`)
+        const upcoming = await upcomingResponse.json();
+
+        forecastElement.style.display = 'block'
+
+        const symbolSpanElement = document.createElement('span');
+        symbolSpanElement.classList.add('condition');
+        symbolSpanElement.classList.add('symbol');
+        symbolSpanElement.textContent = wheaterSymbol[today.forecast.condition]
+
+        const spanWrapper = document.createElement('span');
+        spanWrapper.classList.add('condition');
+
+        const locationNameSpan = createForecastDataSpanElementWithTextContent(today.name);
+        const highLowSpan = createForecastDataSpanElementWithTextContent(`${today.forecast.high}/${today.forecast.low}`);
+        const conditionSpan = createForecastDataSpanElementWithTextContent(today.forecast.condition);
+
+        spanWrapper.append(...[locationNameSpan, highLowSpan, conditionSpan]);
+        currentElement.append(...[symbolSpanElement, spanWrapper]);
+
+        const upcomingDaysForecastInfoSpanWrapper = document.createElement('div');
+        upcomingDaysForecastInfoSpanWrapper.classList.add('forecast-info')
+
+        for (const upcomingDayForecast of upcoming.forecast) {
+            const upcomingForecastSpanWrapper = document.createElement('span');
+            upcomingForecastSpanWrapper.classList.add('upcoming');
+
+            const symbolSpanElement = document.createElement('span');
+            symbolSpanElement.classList.add('symbol');
+            symbolSpanElement.textContent = wheaterSymbol[upcomingDayForecast.condition];
+
+            const highLowSpanElement = createForecastDataSpanElementWithTextContent(`${upcomingDayForecast.high}/${upcomingDayForecast.low}`);
+            const conditionSpanElement = createForecastDataSpanElementWithTextContent(upcomingDayForecast.condition);
+
+            upcomingForecastSpanWrapper.append(...[symbolSpanElement, highLowSpanElement, conditionSpanElement])
+            upcomingDaysForecastInfoSpanWrapper.appendChild(upcomingForecastSpanWrapper);
+        }
+
+        upcomingElement.appendChild(upcomingDaysForecastInfoSpanWrapper);
+    })
+}
+
+
+function createForecastDataSpanElementWithTextContent(text) {
+    const newSpanElement = document.createElement('span');
+    newSpanElement.classList.add('forecast-data');
+    newSpanElement.textContent = text;
+
+    return newSpanElement;
 }
 
 attachEvents();
